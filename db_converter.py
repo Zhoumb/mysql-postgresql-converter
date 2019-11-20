773ede0657fe5575c68f16997ee806ae0a925be3
zhoumb <zhoumb@qq.com>
2019-11-20 14:22:49 +0800

    pyenv shell 3.7.4

--- db_converter.py
+++ db_converter.py
@@ -22,7 +22,8 @@ def parse(input_filename, output_filename):
     if input_filename == "-":
         num_lines = -1
     else:
-        num_lines = int(subprocess.check_output(["wc", "-l", input_filename]).strip().split()[0])
+        num_lines = int(subprocess.check_output(
+            ["wc", "-l", input_filename]).strip().split()[0])
     tables = {}
     current_table = None
     creation_lines = []
@@ -49,7 +50,6 @@ def parse(input_filename, output_filename):
     else:
         input_fh = open(input_filename)
 
-
     output.write("-- Converted by db_converter\n")
     output.write("START TRANSACTION;\n")
     output.write("SET standard_conforming_strings=off;\n")
@@ -70,7 +70,8 @@ def parse(input_filename, output_filename):
             secs_left % 60,
         ))
         logging.flush()
-        line = line.decode("utf8").strip().replace(r"\\", "WUBWUBREALSLASHWUB").replace(r"\0", "").replace(r"\'", "''").replace("WUBWUBREALSLASHWUB", r"\\")
+        line = line.strip().replace(r"\\", "WUBWUBREALSLASHWUB").replace(r"\0", "").replace(r"\'", "''").replace("WUBWUBREALSLASHWUB", r"\\").replace("`", '"')
+        # line = line.decode("utf8").strip().replace(r"\\", "WUBWUBREALSLASHWUB").replace(r"\0", "").replace(r"\'", "''").replace("WUBWUBREALSLASHWUB", r"\\")
         # Ignore comment lines
         if line.startswith("--") or line.startswith("/*") or line.startswith("LOCK TABLES") or line.startswith("DROP TABLE") or line.startswith("UNLOCK TABLES") or not line:
             continue
@@ -84,17 +85,19 @@ def parse(input_filename, output_filename):
                 creation_lines = []
             # Inserting data into a table?
             elif line.startswith("INSERT INTO"):
-                output.write(re.sub(r"([^'])'0000-00-00 00:00:00'", r"\1NULL", line.encode("utf8")) + "\n")
+                output.write(
+                    re.sub(r"([^'])'0000-00-00 00:00:00'", r"\1NULL", line) + "\n")
+                # output.write(re.sub(r"([^'])'0000-00-00 00:00:00'", r"\1NULL", line.encode("utf8")) + "\n")
                 num_inserts += 1
             # ???
             else:
-                print "\n ! Unknown line in main body: %s" % line
+                print("\n ! Unknown line in main body: %s" % line)
 
         # Inside-create-statement handling
         else:
             # Is it a column?
             if line.startswith('"'):
-                useless, name, definition = line.strip(",").split('"',2)
+                useless, name, definition = line.strip(",").split('"', 2)
                 try:
                     type, extra = definition.strip().split(" ", 1)
 
@@ -150,29 +153,38 @@ def parse(input_filename, output_filename):
                 elif type.startswith("enum(") or type.startswith("set("):
 
                     types_str = type.split("(")[1].rstrip(")").rstrip('"')
-                    types_arr = [type_str.strip('\'') for type_str in types_str.split(",")]
+                    # types_arr = [type_str.strip('\'') for type_str in types_str.split(",")]
 
                     # Considered using values to make a name, but its dodgy
                     # enum_name = '_'.join(types_arr)
                     enum_name = "{0}_{1}".format(current_table, name)
 
                     if enum_name not in enum_types:
-                        output.write("CREATE TYPE {0} AS ENUM ({1}); \n".format(enum_name, types_str));
+                        output.write("CREATE TYPE {0} AS ENUM ({1}); \n".format(
+                            enum_name, types_str))
                         enum_types.append(enum_name)
 
                     type = enum_name
 
                 if final_type:
-                    cast_lines.append("ALTER TABLE \"%s\" ALTER COLUMN \"%s\" DROP DEFAULT" % (current_table, name))
-                    cast_lines.append("ALTER TABLE \"%s\" ALTER COLUMN \"%s\" TYPE %s USING CAST(\"%s\" as %s)" % (current_table, name, final_type, name, final_type))
+                    cast_lines.append(
+                        "ALTER TABLE \"%s\" ALTER COLUMN \"%s\" DROP DEFAULT" % (current_table, name))
+                    cast_lines.append("ALTER TABLE \"%s\" ALTER COLUMN \"%s\" TYPE %s USING CAST(\"%s\" as %s)" % (
+                        current_table, name, final_type, name, final_type))
                     if final_default:
-                        cast_lines.append("ALTER TABLE \"%s\" ALTER COLUMN \"%s\" SET DEFAULT %s" % (current_table, name, final_default))
+                        cast_lines.append("ALTER TABLE \"%s\" ALTER COLUMN \"%s\" SET DEFAULT %s" % (
+                            current_table, name, final_default))
                 # ID fields need sequences [if they are integers?]
-                if name == "id" and set_sequence is True:
-                    sequence_lines.append("DROP SEQUENCE IF EXISTS %s_id_seq" % (current_table))
-                    sequence_lines.append("CREATE SEQUENCE %s_id_seq" % (current_table))
-                    sequence_lines.append("SELECT setval('%s_id_seq', max(id)) FROM %s" % (current_table, current_table))
-                    sequence_lines.append("ALTER TABLE \"%s\" ALTER COLUMN \"id\" SET DEFAULT nextval('%s_id_seq')" % (current_table, current_table))
+                if extra.endswith("AUTO_INCREMENT"): # if name == "id" and set_sequence is True:
+                    extra = extra.replace("AUTO_INCREMENT", "")
+                    sequence_lines.append(
+                        "DROP SEQUENCE IF EXISTS %s_id_seq" % (current_table))
+                    sequence_lines.append(
+                        "CREATE SEQUENCE %s_id_seq" % (current_table))
+                    sequence_lines.append("SELECT setval('%s_id_seq', max(id)) FROM %s" % (
+                        current_table, current_table))
+                    sequence_lines.append("ALTER TABLE \"%s\" ALTER COLUMN \"id\" SET DEFAULT nextval('%s_id_seq')" % (
+                        current_table, current_table))
                 # Record it
                 creation_lines.append('"%s" %s %s' % (name, type, extra))
                 tables[current_table]['columns'].append((name, type, extra))
@@ -180,29 +192,37 @@ def parse(input_filename, output_filename):
             elif line.startswith("PRIMARY KEY"):
                 creation_lines.append(line.rstrip(","))
             elif line.startswith("CONSTRAINT"):
-                foreign_key_lines.append("ALTER TABLE \"%s\" ADD CONSTRAINT %s DEFERRABLE INITIALLY DEFERRED" % (current_table, line.split("CONSTRAINT")[1].strip().rstrip(",")))
-                foreign_key_lines.append("CREATE INDEX ON \"%s\" %s" % (current_table, line.split("FOREIGN KEY")[1].split("REFERENCES")[0].strip().rstrip(",")))
+                foreign_key_lines.append("ALTER TABLE \"%s\" ADD CONSTRAINT %s DEFERRABLE INITIALLY DEFERRED" % (
+                    current_table, line.split("CONSTRAINT")[1].strip().rstrip(",")))
+                foreign_key_lines.append("CREATE INDEX ON \"%s\" %s" % (current_table, line.split(
+                    "FOREIGN KEY")[1].split("REFERENCES")[0].strip().rstrip(",")))
             elif line.startswith("UNIQUE KEY \""):
-                index_name      = line.split('"')[1].split('"')[0]
-                index_columns   = line.split("(")[1].split(")")[0]
-                index_lines.append("CREATE UNIQUE INDEX \"%s\" ON %s (%s)" % (index_name, current_table, index_columns))
-                drop_index_lines.append("DROP INDEX IF EXISTS \"%s\"" % index_name)
+                index_name = line.split('"')[1].split('"')[0]
+                index_columns = line.split("(")[1].split(")")[0]
+                index_lines.append("CREATE UNIQUE INDEX \"%s\" ON %s (%s)" % (
+                    index_name, current_table, index_columns))
+                drop_index_lines.append(
+                    "DROP INDEX IF EXISTS \"%s\"" % index_name)
             elif line.startswith("UNIQUE KEY"):
-                index_columns   = line.split("(")[1].split(")")[0]
+                index_columns = line.split("(")[1].split(")")[0]
                 index_lines.append("CREATE UNIQUE INDEX ON %s (%s)" % (current_table, index_columns))
             elif line.startswith("KEY \""):
-                index_name      = line.split('"')[1].split('"')[0]
-                index_columns   = line.split("(")[1].split(")")[0]
-                index_lines.append("CREATE INDEX \"%s\" ON %s (%s)" % (index_name, current_table, index_columns))
-                drop_index_lines.append("DROP INDEX IF EXISTS \"%s\"" % index_name)
+                index_name = line.split('"')[1].split('"')[0]
+                index_columns = line.split("(")[1].split(")")[0]
+                index_lines.append("CREATE INDEX \"%s\" ON %s (%s)" % (
+                    index_name, current_table, index_columns))
+                drop_index_lines.append(
+                    "DROP INDEX IF EXISTS \"%s\"" % index_name)
             elif line.startswith("KEY"):
                 index_columns = line.split("(")[1].split(")")[0]
                 index_lines.append("CREATE INDEX ON %s (%s)" % (current_table, index_columns))
             elif line.startswith("FULLTEXT KEY"):
-                fulltext_keys = " || ' ' || ".join( line.split('(')[-1].split(')')[0].replace('"', '').split(',') )
-                index_lines.append("CREATE INDEX ON %s USING gin(to_tsvector('english', %s))" % (current_table, fulltext_keys))
+                fulltext_keys = " || ' ' || ".join(
+                    line.split('(')[-1].split(')')[0].split(','))
+                index_lines.append("CREATE INDEX ON %s USING gin(to_tsvector('english', %s))" % (
+                    current_table, fulltext_keys))
             # Is it the end of the table?
-            elif line == ");":
+            elif line.startswith(") ENGINE=InnoDB"):
                 output.write("DROP TABLE IF EXISTS \"%s\";\n" % current_table)
                 output.write("CREATE TABLE \"%s\" (\n" % current_table)
                 for i, line in enumerate(creation_lines):
@@ -211,8 +231,7 @@ def parse(input_filename, output_filename):
                 current_table = None
             # ???
             else:
-                print "\n ! Unknown line inside table creation: %s" % line
-
+                print("\n ! Unknown line inside table creation: %s" % line)
 
     # Finish file
     output.write("\n-- Post-data save --\n")
@@ -249,7 +268,7 @@ def parse(input_filename, output_filename):
     # Finish file
     output.write("\n")
     output.write("COMMIT;\n")
-    print ""
+    print("")
 
 
 if __name__ == "__main__":
